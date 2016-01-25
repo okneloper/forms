eb3f755503dcffc53073275510993dbb95fe5947
Aleksey Lavrinenko <aleksey.lavrinenko@mtcmedia.co.uk>
2016-01-26 00:43:37 +0200

    Add Phone element

---
+++ src/Elements/Phone.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Okneloper\Forms\Elements;
+
+use Okneloper\Forms\Element;
+
+class Phone extends Element
+{
+    static protected $defaultAttributes = ['class' => 'text'];
+
+    protected $type = 'phone';
+
+}
