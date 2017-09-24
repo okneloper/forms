74c1db3890a5b7cdd971905bc7beb37bca5bfe59
Aleksey Lavrinenko <okneloper@gmail.com>
2017-09-24 23:34:15 +0300

    Add baset test class

---
+++ tests/TestCase.php
@@ -0,0 +1,12 @@
+<?php
+
+class TestCase extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @return \Okneloper\Forms\Form
+     */
+    public function makeForm()
+    {
+        return new \Okneloper\Forms\Form();
+    }
+}
